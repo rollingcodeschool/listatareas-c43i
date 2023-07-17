214ebe2bd05830181f47bd26a97a532ac372b2a0
Emilse Arias <emilsearias@gmail.com>
2023-07-17 20:31:49 -0300

    almacenamos la tarea en el state tarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,12 +1,20 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] =useState('');
+  const [listaTareas, setListaTareas] =useState([]);
+
   return (
     <>
     <Form>
       <Form.Group className="mb-3 d-flex" controlId="tarea">
-        <Form.Control type="text" placeholder="ingrese una tarea" />
+        <Form.Control type="text" 
+        placeholder="ingrese una tarea"
+        value={tarea} 
+        onChange={(e)=> setTarea(e.target.value) }
+         />
         <Button variant="primary">Agregar</Button>
       </Form.Group>
     </Form>
