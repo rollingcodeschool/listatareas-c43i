16eb9dd5d57a34623f8edd2f0f78474b2deef00a
Emilse Arias <emilsearias@gmail.com>
2023-07-17 21:14:17 -0300

    agregamos las tareas en el array listaTareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -3,22 +3,33 @@ import ListaTareas from "./ListaTareas";
 import { useState } from "react";
 
 const FormularioTarea = () => {
-  const [tarea, setTarea] =useState('');
-  const [listaTareas, setListaTareas] =useState([]);
+  const [tarea, setTarea] = useState("");
+  const [listaTareas, setListaTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //guardar la tarea en el array listaTareas
+   setListaTareas([...listaTareas, tarea]);
+    //limpiar el value del input
+    setTarea('');
+  };
 
   return (
     <>
-    <Form>
-      <Form.Group className="mb-3 d-flex" controlId="tarea">
-        <Form.Control type="text" 
-        placeholder="ingrese una tarea"
-        value={tarea} 
-        onChange={(e)=> setTarea(e.target.value) }
-         />
-        <Button variant="primary">Agregar</Button>
-      </Form.Group>
-    </Form>
-    <ListaTareas></ListaTareas>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex" controlId="tarea">
+          <Form.Control
+            type="text"
+            placeholder="ingrese una tarea"
+            value={tarea}
+            onChange={(e) => setTarea(e.target.value)}
+          />
+          <Button variant="primary" type="submit">
+            Agregar
+          </Button>
+        </Form.Group>
+      </Form>
+      <ListaTareas></ListaTareas>
     </>
   );
 };
